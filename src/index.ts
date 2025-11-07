b0b692be4841a7f8a8d73f7209dd9bf59951289b
Wesley <wesleysantoscardoso2404@gmail.com>
2025-11-07 14:39:29 -0300

    feat: Mostrando onde esta a API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Olá, TypeScript!!");
+import express from "express";
+import aluno from "./routes/alunos";
+import conectaDB from "./database/connection";
+
+const app = express();
+const PORT = 3000;
+
+conectaDB();
+
+app.use(express.json());
+app.use("/alunos", aluno);
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}/alunos`);
+});
